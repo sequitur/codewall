c51b433ca55e34419fe451cb628af16375e67ef6
Bruno Dias <bruno.r.dias@gmail.com>
2017-09-24 22:09:35 -0300

    Add effects and color schemes

--- src/main.js
+++ src/main.js
@@ -4,12 +4,42 @@ import html2canvas from 'html2canvas';
 import {
   CanvasTexture, Scene, OrthographicCamera, WebGLRenderer, Mesh, Clock,
   PlaneGeometry, MeshLambertMaterial, HemisphereLight, PointLight, DoubleSide
-  } from 'three';
-import { EffectComposer, GlitchPass, RenderPass } from 'postprocessing';
+} from 'three';
+import {
+  EffectComposer, GlitchPass, GlitchMode, RenderPass, BloomPass, FilmPass,
+  KernelSize, PixelationPass, DotScreenPass
+} from 'postprocessing';
 
 let currentText;
 let displayRunning;
 let hideComments;
+
+/* Create the Scene */
+const canvas = document.getElementById('draw-canvas');
+const renderer = new WebGLRenderer({ canvas });
+const planeMaterial = new MeshLambertMaterial( {
+  side: DoubleSide
+} );
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const composer = new EffectComposer(renderer, {
+  depthTexture: true,
+});
+
+const frustumSize = 500;
+const aspect = window.innerWidth / window.innerHeight;
+const scene = new Scene();
+const cameraOrtho = new OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 10, 2000 );
+const geometry = new PlaneGeometry( frustumSize * aspect, frustumSize, 16 );
+const plane = new Mesh( geometry, planeMaterial );
+scene.add( plane );
+scene.add(new HemisphereLight(0xffffff, 0x444444));
+const pointLight = new PointLight(0xffffff, 2, 500, 2);
+pointLight.position.set(100, 200, 150);
+scene.add(pointLight);
+cameraOrtho.position.z = 100;
+cameraOrtho.lookAt(scene.position);
+
 
 /* UI Code */
 
@@ -19,34 +49,114 @@ function displayError (errMsg) {
   $("#error").show();
 }
 
-const canvas = document.getElementById('draw-canvas');
-const renderer = new WebGLRenderer({ canvas });
-const planeMaterial = new MeshLambertMaterial( {
-  side: DoubleSide
-} );
-renderer.setSize(window.innerWidth, window.innerHeight);
 
-const composer = new EffectComposer(renderer);
+const EFFECT_STACKS = {
+  gibson (composer) {
+    const glitch = new GlitchPass({
+      dtSize: 512
+    });
+    const scanlines = new FilmPass({
+      vignette: true,
+      eskil: true,
+      scanlineDensity: 14,
+      scanlineIntensity: 0.1,
+      vignetteOffset: 0.75,
+      vignetteDarkness: 2
+    });
+    scanlines.renderToScreen = true;
+    composer.addPass(glitch);
+    composer.addPass(scanlines);
+  },
+
+  sadboy (composer) {
+    const bloom = new BloomPass({
+      kernelSize: KernelSize.SMALL,
+      intensity: 5.0,
+      distinction: 2.0,
+    });
+    const filmGrain = new FilmPass({
+      vignette: true,
+      eskil: true,
+      scanlines: false,
+      vignetteDarkness: 2.0,
+    });
+    filmGrain.renderToScreen = true;
+    composer.addPass(bloom);
+    composer.addPass(filmGrain);
+  },
+
+  scorchedtube (composer) {
+    const bloom = new BloomPass({
+      kernelSize: KernelSize.MEDIUM,
+      intensity: 10.0,
+      distinction: 2.0,
+      screenMode: false,
+    });
+    const scanlines = new FilmPass({
+      vignette: true,
+      eskil: true,
+      scanlineDensity: 14,
+      scanlineIntensity: 0.1,
+      vignetteOffset: 0.75,
+      vignetteDarkness: 4,
+    });
+    scanlines.renderToScreen = true;
+    composer.addPass(bloom);
+    composer.addPass(scanlines);
+  },
+
+  ntef (composer) {
+    const bloom = new BloomPass({
+      kernelSize: KernelSize.SMALL,
+      intensity: 5.0,
+      distinction: 2.0,
+    });
+    const pixel = new PixelationPass(2.5);
+    const scanlines = new FilmPass({
+      vignette: true,
+      eskil: true,
+      scanlineDensity: 14,
+      scanlineIntensity: 0.1,
+      vignetteOffset: 0.75,
+      vignetteDarkness: 2
+    });
+    scanlines.renderToScreen = true;
+    composer.addPass(bloom);
+    composer.addPass(pixel);
+    composer.addPass(scanlines);
+  },
+
+  badlifechoices (composer) {
+    const bloom = new BloomPass({
+      kernelSize: KernelSize.SMALL,
+      intensity: 5.0,
+      distinction: 2.0,
+    });
+    const dots = new DotScreenPass({
+      scale: 1.5
+    });
+    const filmGrain = new FilmPass({
+      vignette: true,
+      eskil: true,
+      scanlines: false,
+      vignetteDarkness: 2.0,
+      noiseIntensity: 0.75
+    });
+    filmGrain.renderToScreen = true;
+    composer.addPass(bloom);
+    composer.addPass(dots);
+    composer.addPass(filmGrain);
+  },
+}
+
+function createEffectComposer (composer, scene, camera) {
+  composer.reset();
+  composer.addPass(new RenderPass(scene, camera));
+  EFFECT_STACKS[$("select[name=aestheticSelect]").val()](composer);
+}
 
 function createWebGLDisplay () {
-  const frustumSize = 500;
-  const aspect = window.innerWidth / window.innerHeight;
-  const scene = new Scene();
-  const cameraOrtho = new OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 10, 2000 );
-  const geometry = new PlaneGeometry( frustumSize * aspect, frustumSize, 16 );
-  const plane = new Mesh( geometry, planeMaterial );
-  scene.add( plane );
-  scene.add(new HemisphereLight(0xffffff, 0x444444));
-  const pointLight = new PointLight(0xffffff, 2, 500, 2);
-  pointLight.position.set(100, 200, 150);
-  scene.add(pointLight);
-  cameraOrtho.position.z = 100;
-  cameraOrtho.lookAt(scene.position);
-
-  composer.addPass(new RenderPass(scene, cameraOrtho));
-  const glitchPass = new GlitchPass();
-  glitchPass.renderToScreen = true;
-  composer.addPass(glitchPass);
+  createEffectComposer(composer, scene, cameraOrtho);
   const clock = new Clock();
 
   function animate () {
@@ -65,6 +175,7 @@ function displayOnCanvas (texCanvas) {
   planeMaterial.map = texture;
   texture.needsUpdate = true;
   if (!displayRunning) createWebGLDisplay();
+  else createEffectComposer(composer, scene, cameraOrtho);
   $("#loading,#url-form,#error").hide();
 }
 
@@ -77,6 +188,8 @@ function displayContent (content) {
   if (hideComments) elem.addClass('hide-comments');
   $("button[name=reset]").show();
   elem.show();
+
+  const aspect = window.innerWidth / window.innerHeight;
   html2canvas(elem[0]).then(displayOnCanvas);
 }
 
@@ -136,6 +249,10 @@ const optionActions = {
   styleSelect (val) {
     $("#content").removeClass(STYLE_CLASSES);
     $("#content").addClass(val);
+  },
+
+  aestheticSelect () {
+    // NOOP
   },
 
   fontSize (val) {
