e476292b7547674c0e3e3e51c4cfbcb76e3edac0
Bruno Dias <bruno.r.dias@gmail.com>
2017-09-25 17:59:00 -0300

    General code cleanup and comments, fix Firefox issue

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ import {
   KernelSize, PixelationPass, DotScreenPass
 } from 'postprocessing';
 
-// Ugly global flags
+// Ugly global vars
 let currentText;
 let displayRunning;
 let hideComments;
@@ -27,39 +27,50 @@ let hideComments;
 /* Create the Scene */
 const canvas = document.getElementById('draw-canvas');
 const renderer = new WebGLRenderer({ canvas });
-const planeMaterial = new MeshLambertMaterial( {
-  side: DoubleSide
-} );
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-const composer = new EffectComposer(renderer, {
-  depthTexture: true,
-});
+const composer = new EffectComposer(renderer);
 
+// The frustum size just sets the arbitrary unit scale of our scene.
 const frustumSize = 500;
 const aspect = window.innerWidth / window.innerHeight;
+
 const scene = new Scene();
-const cameraOrtho = new OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 10, 2000 );
+
+// The orthographic camera's frustum (viewing area) is just a rectangular box,
+// unlike the pyramid-shaped frustum of a perspective camera. The four
+// parameters are the distances between the left, right, top, and bottom sides
+// from the center, which is initially at (0, 0, 0).
+const cameraOrtho = new OrthographicCamera(
+  frustumSize * aspect / - 2,
+  frustumSize * aspect / 2,
+  frustumSize / 2,
+  frustumSize / - 2, 10, 2000
+);
+
+// We want to render a 2D scene (our code) in 3D space. So we create a plane.
+// Because the plane is the same size as the camera's frustum and the camera
+// is orthographic, it will fill the entire view perfectly.
+const planeMaterial = new MeshLambertMaterial({ side: DoubleSide });
 let geometry = new PlaneGeometry( frustumSize * aspect, frustumSize, 16 );
-let plane = new Mesh( geometry, planeMaterial );
-scene.add( plane );
+let plane = new Mesh(geometry, planeMaterial);
+scene.add(plane);
+
+// Our plane uses a diffuse shader so it has to be lit for us to see what's on
+// it. This achieves a slight gradient effect over the whole image without
+// me having to write a shader to do it.
 scene.add(new HemisphereLight(0xffffff, 0x444444));
 const pointLight = new PointLight(0xffffff, 2, 500, 2);
 pointLight.position.set(100, 200, 150);
 scene.add(pointLight);
+
+// Move the camera back a little bit so it can see the plane.
 cameraOrtho.position.z = 100;
 cameraOrtho.lookAt(scene.position);
 
-
-/* UI Code */
-
-function displayError (errMsg) {
-  $("#error-msg").text(errMsg);
-  $("#url-form,#loading,#content").hide();
-  $("#error").show();
-}
-
-
+// The different a e s t h e t i c s are different "stacks" of postprocessing
+// effects, implemented here as functions. Every time we redraw we rebuild the
+// effect stack according to what's selected in the UI.
 const EFFECT_STACKS = {
   gibson (composer) {
     const glitch = new GlitchPass({
@@ -159,12 +170,16 @@ const EFFECT_STACKS = {
   },
 }
 
+// Set up the composer to use the currently-chosen effects stack.
 function createEffectComposer (composer, scene, camera) {
   composer.reset();
+  // Every effects stack starts by just rendering the scene.
   composer.addPass(new RenderPass(scene, camera));
   EFFECT_STACKS[$("select[name=aestheticSelect]").val()](composer);
 }
 
+// Fire up the WebGL canvas. We do this once we have loaded some content to use
+// in it.
 function createWebGLDisplay () {
   createEffectComposer(composer, scene, cameraOrtho);
   const clock = new Clock();
@@ -178,7 +193,26 @@ function createWebGLDisplay () {
   displayRunning = true;
 }
 
+// Once content has been fetched from the network, show it.
+function displayContent (content) {
+  const elem = $("#content");
+  // Highlight the content with highlight.js
+  const highlightedContent = hl.highlightAuto(content).value
+  // Just in case, compress all whitespace into single spaces.
+  const fixedContent = highlightedContent.replace(/\s+/g, " ");
+  // Insert the content into html.
+  elem.html(fixedContent);
+  elem.attr("class", $("select[name=styleSelect]").val());
+  if (hideComments) elem.addClass('hide-comments');
+  // We have to show the element so html2canvas will properly work.
+  elem.show();
+  html2canvas(elem[0]).then(displayOnCanvas);
+}
 
+// html2canvas returns a promise which returns a canvas that contains
+// an image of our code, which we turned into a DOM tree of styled spans
+// with hl.js. Then we want to use that canvas as a texture for the 3D plane
+// in our webGL canvas, which the user actually sees.
 function displayOnCanvas (texCanvas) {
   $('#content').hide();
   const texture = new CanvasTexture(texCanvas);
@@ -187,20 +221,14 @@ function displayOnCanvas (texCanvas) {
   if (!displayRunning) createWebGLDisplay();
   else createEffectComposer(composer, scene, cameraOrtho);
   $("#loading,#url-form,#error").hide();
+  $("button[name=reset]").show();
 }
 
-function displayContent (content) {
-  const elem = $("#content");
-  const highlightedContent = hl.highlightAuto(content).value
-  const fixedContent = highlightedContent.replace(/\s+/g, " ");
-  elem.html(fixedContent);
-  elem.attr("class", $("select[name=styleSelect]").val());
-  if (hideComments) elem.addClass('hide-comments');
-  $("button[name=reset]").show();
-  elem.show();
-
-  const aspect = window.innerWidth / window.innerHeight;
-  html2canvas(elem[0]).then(displayOnCanvas);
+/* UI functions relating to various buttons and widgets */
+function displayError (errMsg) {
+  $("#error-msg").text(errMsg);
+  $("#url-form,#loading,#content").hide();
+  $("#error").show();
 }
 
 function reset () {
@@ -217,7 +245,10 @@ function obj2InlineStyle (obj) {
   return styles.join(' ');
 }
 
-/* Get file from github */
+// Get a file from github
+// This is done in the least smart way imaginable, by just figuring out what
+// the raw URL is, instead of using Github's API. So it's not guaranteed to
+// work in the future, but it's good enough for a hack.
 function getUrl (url) {
   return fetch(
     url.replace(/^https?:\/\/github.com/, "https://raw.githubusercontent.com")
@@ -231,19 +262,26 @@ function getUrl (url) {
   });
 }
 
-$('#try-again').on('click', function () {
+// Go back to the URL form if you want to retry.
+$('#try-again').on('click', function tryAgain () {
   $('#error').hide();
   $('#url-form').show();
 });
 
-$("#url-form").on("submit", function (event) {
+// Get an URL and read it.
+$("#url-form").on("submit", function tryURL (event) {
   event.preventDefault();
   $("#url-form").hide();
   $("#loading").show();
   getUrl($("#url-form input").val())
     .then(blob => {
-      if (blob.type !== "text/plain")
-        throw new Error("This doesn't appear to be a text file.");
+      // We use pattern matching here because some browsers (Firefox) include
+      // the encoding in the blob type, while others do not.
+      if (!blob.type.match(/^text\/plain/))
+        throw new Error(
+          `This doesn't appear to be a text file. Expected text/plain, received
+          ${blob.type}.`
+        );
       const reader = new FileReader;
       reader.addEventListener("loadend", () => {
         currentText = reader.result;
@@ -254,8 +292,10 @@ $("#url-form").on("submit", function (event) {
     .catch(displayError);
 });
 
-const STYLE_CLASSES = "venturis weyland-yutani";
+const STYLE_CLASSES = "venturis weyland-yutani delos cyberdyne tyrell";
 
+// How to respond to something changing in the style menu.
+// I love callup
 const optionActions = {
   styleSelect (val) {
     $("#content").removeClass(STYLE_CLASSES);
@@ -279,13 +319,18 @@ const optionActions = {
   }
 }
 
-$("#style-menu select,#style-menu input").on("change", function (event) {
-  const {name, value} = event.target;
-  optionActions[name](value);
-  if (!displayRunning) return;
-  $("button[name=redraw]").show();
+$("#style-menu select,#style-menu input").on("change",
+  function respondToChange (event) {
+    const {name, value} = event.target;
+    optionActions[name](value);
+    if (!displayRunning) return;
+    $("button[name=redraw]").show();
 });
 
+// Redraw when requested by the user
+// We don't do this automatically because it takes a long time and blocks the
+// UI
+// We could do it in a web worker but [shrug emoji]
 function redraw () {
   $("#url-form").hide();
   $("#loading").show();
@@ -299,6 +344,7 @@ function redraw () {
 $("button[name=reset]").on("click", reset);
 $("button[name=redraw]").on("click", redraw);
 
+// When the window gets resized, we have to resize our scene too.
 window.onresize = function () {
   const {innerWidth: width, innerHeight: height} = window;
   const aspect = width / height;
@@ -308,6 +354,9 @@ window.onresize = function () {
   cameraOrtho.bottom = - frustumSize / 2;
   cameraOrtho.updateProjectionMatrix();
   renderer.setSize(width, height);
+
+  // Easier to generate a new plane and replace the old one than to worry about
+  // scaling the same plane.
   scene.remove(plane);
   geometry = new PlaneGeometry( frustumSize * aspect, frustumSize, 16 );
   plane = new Mesh( geometry, planeMaterial );
